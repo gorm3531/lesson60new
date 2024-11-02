5b0a94b2f93bcc315907874402b5481270051d37
gorm3531 <mcgermafrodit@gmail.com>
2024-11-02 12:57:39 +0700

    module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import asyncio
-api = '7705832901:AAFlAOrIVady3Xcx3X9VWeIqm2U7EFVIsKg'
+api = '***'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
